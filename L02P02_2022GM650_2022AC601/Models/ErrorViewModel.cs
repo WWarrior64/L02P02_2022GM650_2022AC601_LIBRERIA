7c663eed652ea7d65a3bbfa345238cf2066a5395
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-04-03 17:45:43 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022GM650_2022AC601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022GM650_2022AC601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
