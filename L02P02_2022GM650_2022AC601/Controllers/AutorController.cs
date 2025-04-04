273bd86d7374c87e466f5089c13217a1b5b9c476
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-04-03 18:33:45 -0600

    Vista de prototipo 1

---
+++ L02P02_2022GM650_2022AC601/Controllers/AutorController.cs
@@ -0,0 +1,30 @@
+﻿using L02P02_2022GM650_2022AC601.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2022GM650_2022AC601.Controllers
+{
+    public class AutorController : Controller
+    {
+        private readonly libreriaDbContext _context;
+        public AutorController(libreriaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var listadoAutores = _context.autores
+                .Select(a => new
+                {
+                    Id = a.id,
+                    Nombre = a.autor
+                })
+                .ToList();
+
+            ViewData["listadoAutores"] = listadoAutores;
+
+            return View();
+        }
+    }
+}
